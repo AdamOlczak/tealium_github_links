f043678d27505c302f0c53f0df6c5e1beb858afd
Adam Olczak <aolczak@okaeri.pl>
2019-09-24 11:12:13 +0200

    convert indetation to spaces Add rewire module remove module.export from first.js rewrite test to use rewire module

--- src/first.js
+++ src/first.js
@@ -1,10 +1,8 @@
 var TMSHelper = {
-	toLower: function (input) {
-		return input.toLowerCase();
-	},
-	toUpper: function (input) {
-		return input.toUpperCase() + 'AAA';
-	}
-};
-
-module.exports = TMSHelper;
+  toLower: function (input) {
+    return input.toLowerCase()
+  },
+  toUpper: function (input) {
+    return input.toUpperCase()
+  }
+}
--- test/first.spec.js
+++ test/first.spec.js
@@ -1,17 +1,17 @@
-//define(['chai', './first.js'], function(chai, TMSHelper) {
-	var expect = require('chai').expect;
-	var TMSHelper = require('../src/first.js');
-	describe('TMSHelper', function() {
-		describe('#toUpper()', function () {
-			it('should conver strings to lowercase correctlly', function () {
-				var str = TMSHelper.toLower('TEST');
-				expect(str).to.equal('test');
-			});
-		});
-		describe('#toUpper()', function () {
-			it('should convert strings to uppercase correctly', function () {
-				expect(TMSHelper.toUpper('lorem ipsum')).to.equal('LOREM IPSUM');
-			});
-		});
-	});
-//});
+var expect = require('chai').expect
+var rewire = require('rewire')
+var firstModule = rewire('../src/first.js')
+describe('TMSHelper', function () {
+  var TMSHelper = firstModule.__get__('TMSHelper')
+  describe('#toUpper()', function () {
+    it('should conver strings to lowercase correctlly', function () {
+      var str = TMSHelper.toLower('TEST')
+      expect(str).to.equal('test')
+    })
+  })
+  describe('#toUpper()', function () {
+    it('should convert strings to uppercase correctly', function () {
+      expect(TMSHelper.toUpper('lorem ipsum')).to.equal('LOREM IPSUM')
+    })
+  })
+})
